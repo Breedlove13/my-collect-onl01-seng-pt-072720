5d6d4872f5b5c945db81109714a296f7e7b620cd
Brian Breedlove <b.breedlove@outlook.com>
2020-07-29 17:40:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   n_collection = []
   while i < collection.length 
-  n_collection.push(yield collection[i])
+  n_collection << yield collection[i])
   i += 1 
   end
 n_collection
